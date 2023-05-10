e3227f148385a0a2cf768971863a02a9d9d62230
DominicLin1 <dominiclin777@gmail.com>
2023-05-10 10:10:55 -0400

    yay

--- sql-project/src/views/HomeView.vue
+++ sql-project/src/views/HomeView.vue
@@ -1,4 +1,9 @@
 <script setup>
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://tzithwsneecztaewiwhj.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
