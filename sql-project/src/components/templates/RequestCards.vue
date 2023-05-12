ad97869b903265723be683622b28920ff545bb10
LeoDwi <leod13579@gmail.com>
2023-05-12 09:57:38 -0400

    card template

---
+++ sql-project/src/components/templates/RequestCards.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'RequestCard',
+  props: {
+    appointmentTime: String,
+    toDoList: String,
+    remove: Function
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <h2 class="name">Appointment at: {{ appointmentTime }}</h2>
+    <p class="description">To Do: {{ toDoList }}</p>
+    <button
+      v-on:click="
+        {
+          {
+            remove
+          }
+        }
+      "
+    ></button>
+  </div>
+</template>
---
+++ sql-project/src/views/RequestLogView.vue
@@ -0,0 +1,7 @@
+<script>
+import RequestCard from '../components/templates/RequestCards.vue'
+export default {
+  components: { RequestCard },
+  data() {}
+}
+</script>
