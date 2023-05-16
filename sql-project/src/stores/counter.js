dd6a9b8764ab0762da358b7eedf109c768115217
Ryan Chen <ryanchen979@gmail.com>
2023-05-16 10:15:41 -0400

    productive day
    hehehehaw

--- sql-project/src/stores/counter.js
+++ sql-project/src/stores/counter.js
@@ -1,12 +1,16 @@
-import { ref, computed } from 'vue'
+// import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useStore = defineStore('storeId', {
+  // arrow function recommended for full type inference
+  state: () => {
+    return {
+      // all these properties will have their type inferred automatically
+      count: 0,
+      name: 'Eduardo',
+      isAdmin: true,
+      items: [],
+      hasChanged: true
+    }
   }
-
-  return { count, doubleCount, increment }
 })
--- sql-project/src/views/HomeView.vue
+++ sql-project/src/views/HomeView.vue
@@ -6,48 +6,47 @@ const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aXRod3NuZWVjenRhZXdpd2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2Mzk5MjksImV4cCI6MTk5OTIxNTkyOX0.YeSE7Cuk2UX5jD6haxAnmM_-RdlssSRtowQH9ejl_1w'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-// let { data: user_logins, error } = await supabase
-//   .from('user_logins')
-//   .select('username')
-// console.log(data)
+/* let { data: user_logins, error } = await supabase
+  .from('user_logins')
+  .select('username')
+console.log(data) */
 
 export default {
-  methods:  function () {
-    document.querySelector('signup-submit').addEventListener('submit', async function () {
-      let userEmail = document.querySelector('.signup-email').value
-      let userPassword = document.querySelector('.signup-password').value
+  data() {},
+  methods: {
+    async signup() {
+      let userEmail = document.getElementById('signup-email').value
+      let userPassword = document.getElementById('signup-password').value
 
       console.log(userEmail)
       console.log(userPassword)
 
-      const { data, error } = await supabase
-      .from('user_logins')
-      .insert([{ email: 'userEmail', password: 'userPassword' }])
-    })
+      if (userEmail === '' || userPassword === '') {
+        console.log('error')
+      } else {
+        const { data, error } = await supabase
+          .from('user_logins')
+          .insert([{ email: userEmail, password: userPassword }])
+      }
 
-    
+      /* let { data, error } = await supabase.auth.signUp({
+          email: userEmail,
+          password: userPassword
+        }) */
+    }
   }
 }
 </script>
 
 <template>
   <main>
-    <div class="login-card">
-      <h2>Login</h2>
-      <input type="text" placeholder="email" />
-      <br />
-      <input type="text" placeholder="password" />
-      <br />
-      <button class="submit">Login</button>
-    </div>
-
     <div class="signup-card">
       <h2>Sign Up</h2>
-      <input class="signup-email" type="text" placeholder="email" />
+      <input type="text" id="signup-email" placeholder="email" required />
       <br />
-      <input class="signup-password" type="text" placeholder="password" />
+      <input type="text" id="signup-password" placeholder="password" required />
       <br />
-      <button class="signup-submit">Sign Up</button>
+      <input type="submit" value="submit" id="signup-submit" @click="signup" />
     </div>
   </main>
 </template>
