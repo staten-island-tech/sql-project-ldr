9c4e6f934bb7466d8596369034bad8365cfc4e8a
Ryan Chen <ryanchen979@gmail.com>
2023-05-17 10:16:04 -0400

    something

--- sql-project/src/views/HomeView.vue
+++ sql-project/src/views/HomeView.vue
@@ -6,33 +6,32 @@ const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aXRod3NuZWVjenRhZXdpd2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2Mzk5MjksImV4cCI6MTk5OTIxNTkyOX0.YeSE7Cuk2UX5jD6haxAnmM_-RdlssSRtowQH9ejl_1w'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-/* let { data: user_logins, error } = await supabase
-  .from('user_logins')
-  .select('username')
-console.log(data) */
-
 export default {
   data() {},
   methods: {
-    async signup() {
+    async signup(a) {
+      a.preventDefault()
+
+      let name = document.getElementById('signup-name').value
       let userEmail = document.getElementById('signup-email').value
       let userPassword = document.getElementById('signup-password').value
 
+      console.log(name)
       console.log(userEmail)
       console.log(userPassword)
 
-      if (userEmail === '' || userPassword === '') {
+      if (name == '' || userEmail == '' || userPassword == '') {
         console.log('error')
       } else {
         const { data, error } = await supabase
           .from('user_logins')
           .insert([{ email: userEmail, password: userPassword }])
-      }
 
-      /* let { data, error } = await supabase.auth.signUp({
+        /* let { data, error } = await supabase.auth.signUp({
           email: userEmail,
           password: userPassword
         }) */
+      }
     }
   }
 }
@@ -40,23 +39,38 @@ export default {
 
 <template>
   <main>
-    <div class="signup-card">
-      <h2>Sign Up</h2>
-      <input type="text" id="signup-email" placeholder="email" required />
-      <br />
-      <input type="text" id="signup-password" placeholder="password" required />
-      <br />
-      <input type="submit" value="submit" id="signup-submit" @click="signup" />
-    </div>
+    <form class="signup-card">
+      <div class="name-inputs">
+        <div class="input">
+          <label for="first-name">First Name</label>
+          <input type="text" id="signup-name" placeholder="name" required />
+        </div>
+
+        <div class="input">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="signup-name" placeholder="name" required />
+        </div>
+      </div>
+
+      <div class="input">
+        <label for="email">Email</label>
+        <input type="text" id="signup-email" placeholder="email" required />
+      </div>
+
+      <div class="input">
+        <label for="password">Password</label>
+        <input type="text" id="signup-password" placeholder="password" required />
+      </div>
+
+      <input type="submit" value="Sign Up" id="signup-submit" @click="signup" />
+    </form>
   </main>
 </template>
 
 <style scoped>
-.login-card {
-  width: 20%;
-  /* border: solid 2px red; */
-}
-.submit {
-  width: 11rem;
+.input {
+  display: flex;
+  flex-direction: column;
+  width: 10rem;
 }
 </style>
