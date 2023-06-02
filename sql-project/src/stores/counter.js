4b5f72fc7f4f0b8963f8a7d97059eb1a170e30e4
DominicLin1 <dominiclin777@gmail.com>
2023-06-02 10:26:52 -0400

    send this to ryan

--- sql-project/src/stores/counter.js
+++ sql-project/src/stores/counter.js
@@ -6,20 +6,30 @@ const supabaseUrl = 'https://tzithwsneecztaewiwhj.supabase.co'
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aXRod3NuZWVjenRhZXdpd2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2Mzk5MjksImV4cCI6MTk5OTIxNTkyOX0.YeSE7Cuk2UX5jD6haxAnmM_-RdlssSRtowQH9ejl_1w'
 const supabase = createClient(supabaseUrl, supabaseKey)
+export const useAuthStore = defineStore({
+  id: 'auth',
+  state: () => ({
+      // initialize state from local storage to enable user to stay logged in
+      user: JSON.parse(localStorage.getItem('user')),
+      returnUrl: null
+  }),
+})
 export const useStore = defineStore('storeId', {
   // arrow function recommended for full type inference
   state: () => {
     return {
       // all these properties will have their type inferred automatically
       user: {},
-      signedIn: false,
-      signedOut: true,
-      appointments: []
-    }
-  },
+   
+    }},
+   
   actions: {
     async register(user) {
-      await supabase.auth.getUser()
+     const user=await supabase.auth.getUser
+     this.user=user
+     localStorage.setItem('user', JSON.stringify(user));
+console.log(this.register)
     }
+    
   } 
 })
